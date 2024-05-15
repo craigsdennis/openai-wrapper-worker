150f1f512262f92c44ee2dca1f46b31ad01b814c
Craig Dennis <craigsdennis@gmail.com>
2024-05-15 10:58:32 -0600

    Adds completions and embeddigns

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {Hono} from "hono";
+import OpenAI from "openai";
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+
+const app = new Hono();
+
+app.get("/completions", async (c) => {
+	const openai = new OpenAI({
+		apiKey: c.env.CLOUDFLARE_API_KEY,
+		baseURL: `https://api.staging.cloudflare.com/client/v4/accounts/${c.env.CLOUDFLARE_ACCOUNT_ID}/ai/v1/`
+	});
+
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [{ role: "user", content: "Make some robot noises" }],
+		model: "@cf/meta/llama-3-8b-instruct",
+	});
+	return c.json(chatCompletion);
+});
+
+app.get("/embeddings", async (c) => {
+	const openai = new OpenAI({
+		apiKey: c.env.CLOUDFLARE_API_KEY,
+		baseURL: `https://api.staging.cloudflare.com/client/v4/accounts/${c.env.CLOUDFLARE_ACCOUNT_ID}/ai/v1/`
+	});
+
+	const embeddings = await openai.embeddings.create({
+		model: "@cf/baai/bge-large-en-v1.5",
+		input: "I'm a little teapot"
+	});
+	return c.json(embeddings);
+})
+
+
+
+export default app;
